cdd6c89a45c13d76d782b69c25bcbd4ae04d0e34
Cole Markham <github@ccmcomputing.net>
2018-05-21 12:50:36 -0500

    Add eclipse project, add dump endpoint

--- src/main/java/net/ccmcomputing/webhook/LoggerWebhook.java
+++ src/main/java/net/ccmcomputing/webhook/LoggerWebhook.java
@@ -1,11 +1,14 @@
 package net.ccmcomputing.webhook;
+
 import static spark.Spark.*;
 
-public class LoggerWebhook {
-    public static void main(String[] args) {
-        get("/hello", (req, res) -> "Hello World");
-        post("/dump", (req, res) -> {
-
-	});
-    }
+public class LoggerWebhook{
+   public static void main(String[] args){
+      get("/hello", (req, res) -> "Hello World");
+      post("/dump", (req, res) -> {
+         String body = req.body();
+         System.out.println(body);
+         return "";
+      });
+   }
 }
